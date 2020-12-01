50a28fa850665234b55d47f4d6ec7da49b046e30
Malik Waqar Ul Hassan <malikwaqarofficial@gmail.com>
2020-12-02 02:44:52 +0500

    Navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,61 @@
 import './App.css';
-import Home from './components/Home/Home';
+import { ReactNavbar } from "react-responsive-animate-navbar";
+import logo from './components/images/logo.png';
+import Intro from './components/Home/Intro';
+import Albums from './components/Home/Albums';
+import Biography from './components/Home/Biography';
+import Contact from './components/Home/Contact';
+import Newsletter from './components/Home/Newsletter';
+import Schedule from './components/Home/Schedule';
+import Tickets from './components/Home/Tickets';
+import Footer from './components/Home/Footer';
 
 function App() {
   return (
     <div className="app">
-      <Home />
+      <ReactNavbar
+        // color="rgb(25, 25, 25)"
+        logo={logo}
+        menu={[
+          { name: "HOME", to: "#" },
+          { name: "TICKETS", to: "#tickets" },
+          { name: "BIOGRAPHY", to: "#biography" },
+          { name: "SCHEDULE", to: "#schedule" },
+          { name: "ALBUMS", to: "#albums" },
+          { name: "CONTACT", to: "#contact" },
+        ]}
+        social={[
+          {
+            name: "Linkedin",
+            url: "https://www.linkedin.com/in/nazeh-taha/",
+            icon: ["fab", "linkedin-in"],
+          },
+          {
+            name: "Facebook",
+            url: "https://www.facebook.com/nazeh200/",
+            icon: ["fab", "facebook-f"],
+          },
+          {
+            name: "Instagram",
+            url: "https://www.instagram.com/nazeh_taha/",
+            icon: ["fab", "instagram"],
+          },
+          {
+            name: "Twitter",
+            url: "http://nazehtaha.herokuapp.com/",
+            icon: ["fab", "twitter"],
+          },
+        ]}
+      />
+      <Intro id="home" />
+      <Tickets id="tickets" />
+      <Biography id="biography" />
+      <Schedule id="schedule" />
+      <Albums id="albums" />
+      <Contact id="contact" />
+      <Newsletter id="newsletter" />
+      <Footer id="footer" />
+      
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,30 +1,28 @@
-import React from 'react';
-import Albums from './Albums';
-import Biography from './Biography';
-import Contact from './Contact';
-import './Home.css';
-import Intro from './Intro';
-import Newsletter from './Newsletter';
-import Schedule from './Schedule';
-import Tickets from './Tickets';
-import Footer from './Footer';
-import Navigation from './Navigation';
+// import React from 'react';
+// import Albums from './Albums';
+// import Biography from './Biography';
+// import Contact from './Contact';
+// import './Home.css';
+// import Intro from './Intro';
+// import Newsletter from './Newsletter';
+// import Schedule from './Schedule';
+// import Tickets from './Tickets';
+// import Footer from './Footer';
 
-const Home = () => {
-    return (
-        <div className="home">
-            {/* <Navigation /> */}
-            <Intro />
-            <Tickets />
-            <Biography />
-            <Schedule />
-            <Albums />
-            <Contact />
-            <Newsletter />
-            <Footer />
-        </div>
+// const Home = () => {
+//     return (
+//         <div className="home">
+//             <Intro />
+//             <Tickets />
+//             <Biography />
+//             <Schedule />
+//             <Albums />
+//             <Contact />
+//             <Newsletter />
+//             <Footer />
+//         </div>
         
-    )
-}
+//     )
+// }
 
-export default Home;
+// export default Home;
